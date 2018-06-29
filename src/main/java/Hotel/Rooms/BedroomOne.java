23cb96d693a1a8f0bddf88278499a635690dd84b
David A Pears <davidapears@gmail.com>
2018-06-29 14:53:27 +0100

    First test on BedroomOne passed

--- src/main/java/Hotel/Rooms/BedroomOne.java
+++ src/main/java/Hotel/Rooms/BedroomOne.java
@@ -5,12 +5,17 @@ public class BedroomOne extends Room {
 
     private String guest;
 
-    public BedroomOne(String roomName, String roomType, double roomRate, int capacity)
-    {
-        super("BedroomOne", "Double", 79.99, 2);
-        this.guest = guest;
+    public BedroomOne(String roomName, String roomType, double roomRate, int capacity, String guest) {
+        super(roomName, roomType, roomRate, capacity);{
+            this.guest = guest;
+        }
+
     }
 
-}
+    public String getGuest(){
+        return this.guest;
+    }
 
+
+}
 
